8231be3dd7352ecb9bd7ef4c927ea7e1d5a515ec
Sid <sidbanks@gmail.com>
2022-06-01 02:12:52 -0500

    Added event listeners for numerical section of key pad.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
-//Basic Mathmatical functions
+//Declaration of keypad variables
+let a;
+let b;
+let output;
+let input;
 
+//Basic Mathmatical functions
 function add(a, b) {
     return a + b;
 }
@@ -18,7 +23,18 @@ function division(a, b) {
 
 // Operate function. Takes input from html page and calls an 
 // operator function.
+function operate() {
 
-function opperate() {
-    
 }
+
+//Event listeners for buttons
+document.getElementById('1').addEventListener('click', function(){ input = input.concat(1); });
+document.getElementById('2').addEventListener('click', function(){ input = input.concat(2); });
+document.getElementById('3').addEventListener('click', function(){ input = input.concat(3); });
+document.getElementById('4').addEventListener('click', function(){ input = input.concat(4); });
+document.getElementById('5').addEventListener('click', function(){ input = input.concat(5); });
+document.getElementById('6').addEventListener('click', function(){ input = input.concat(6); });
+document.getElementById('7').addEventListener('click', function(){ input = input.concat(7); });
+document.getElementById('8').addEventListener('click', function(){ input = input.concat(8); });
+document.getElementById('9').addEventListener('click', function(){ input = input.concat(9); });
+document.getElementById('0').addEventListener('click', function(){ input = input.concat(0); });
